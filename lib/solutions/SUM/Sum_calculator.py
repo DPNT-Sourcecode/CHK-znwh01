971e9447a6c7500a6c22d1967b63f87bc8830279
sbx_user1051 <sbx_user1051@169.254.234.141>
2022-07-09 14:57:32 +0000

    Sat Jul 09 14:57:32 UTC 2022

---
+++ lib/solutions/SUM/Sum_calculator.py
@@ -0,0 +1,5 @@
+class Sum_calculator:
+    def compute(x,y):
+        if(x >= 0 and x <= 100 and y >= 0 and y <= 100):
+            return(x+y)
+        else: print("Please enter numbers between 0 and 100")
