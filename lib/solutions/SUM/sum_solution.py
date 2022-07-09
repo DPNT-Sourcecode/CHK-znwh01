5a2e6379eadfd3b323ebc8371da3cc6808acb652
sbx_user1051 <sbx_user1051@169.254.234.141>
2022-07-09 15:01:32 +0000

    Sat Jul 09 15:01:32 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,9 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
+class Sum_calculator:
+    def compute(x,y):
+        if(x >= 0 and x <= 100 and y >= 0 and y <= 100):
+            return(x+y)
+        else: print("Please enter numbers between 0 and 100")
 def compute(x, y):
     raise NotImplementedError()
+
