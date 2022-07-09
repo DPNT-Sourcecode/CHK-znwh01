63193926d2fc4adfd9696bf66ec4770e9c983834
sbx_user1051 <sbx_user1051@169.254.27.105>
2022-07-09 20:57:08 +0000

    Sat Jul 09 20:57:08 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,10 +15,21 @@ def checkout(skus):
             return price
         else: return -1
     elif item == "B":
-        if amount != 3 and amount > 0:
-            price = amount*50
+        if amount != 2 and amount > 0:
+            price = amount*30
             return price
-        if amount == 3:
-            price = 130
+        if amount == 2:
+            price = 45
             return price
         else: return -1
+    elif item == "C":
+        if amount > 0:
+            price = amount*20
+            return price
+        else: return -1
+    elif item == "D":
+        if amount > 0:
+            price = amount*15
+            return price
+        else: return -1
+    else: return -1
