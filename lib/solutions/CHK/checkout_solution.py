b56b6a1bfc0de53c59784cd7e3263a0e2d2f1290
sbx_user1051 <sbx_user1051@169.254.27.105>
2022-07-09 21:06:08 +0000

    Sat Jul 09 21:06:08 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,37 +4,18 @@
 # skus = unicode string
 def checkout(skus):
     price = 0
-    amount = skus[0]
-    item = skus[1]
-    if item == "A":
-        if amount != 3 and amount > 0:
-            price = amount*50
-            return price
-        if amount == 3:
-            price = 130
-            return price
-        else:
-            return -1
-    elif item == "B":
-        if amount != 2 and amount > 0:
-            price = amount*30
-            return price
-        if amount == 2:
-            price = 45
-            return price
-        else:
-            return -1
-    elif item == "C":
-        if amount > 0:
-            price = amount*20
-            return price
-        else:
-            return -1
-    elif item == "D":
-        if amount > 0:
-            price = amount*15
-            return price
-        else:
-            return -1
-    else:
-        return -1
+    i = 0
+    A, B, C, D = 0
+    while i < len(skus):
+        if i == "A":
+            A = A + 1
+        if i == "B":
+            B = B + 1
+        if i == "C":
+            C = C + 1
+        if i == "D":
+            D = D + 1
+
+    if A ==
+
+
